520a49cb94762a62873f8382d9602a7ab0687509
Christine Emrich <christine.emrich@web.de>
2019-03-09 20:49:59 +0100

    Load data from external json

--- js/main.js
+++ js/main.js
@@ -1,30 +1,27 @@
-var geodata = {
-    "type": "Feature",
-    "properties": {
-        "name": "Name",
-        "popupContent": "Popup Content"
-    },
-    "geometry": {
-        "type": "Point",
-        "coordinates": [8.63896, 49.85672]
-    }
-};
+(function () {
 
-var map = new L.map('map');
-map.setView([49.85672, 8.63896], 16);
+	var map = new L.map('map');
+	map.setView([49.85672, 8.63896], 16);
 
-var osm = new L.StamenTileLayer("toner");
-map.addLayer(osm);
+	var osm = new L.StamenTileLayer("toner");
+	map.addLayer(osm);
 
-
-function onEachFeature(feature, layer) {
-	// does this feature have a property named popupContent?
-	if (feature.properties && feature.properties.popupContent) {
-		layer.bindPopup(feature.properties.name + "<br>" + feature.properties.popupContent);
+	function onEachFeature(feature, layer) {
+		// does this feature have a property named popupContent?
+		if (feature.properties && feature.properties.popupContent) {
+			layer.bindPopup(feature.properties.name + "<br>" + feature.properties.popupContent);
+		}
 	}
-}
 
-// load geo data
-L.geoJSON(geodata, {
-	onEachFeature: onEachFeature
-}).addTo(map);
+	function onGeodataLoaded(json) {
+		console.log(json);
+		var dataLayer = L.geoJSON(json, { onEachFeature: onEachFeature });
+		dataLayer.addTo(map);
+	}
+
+	// load geo data
+	fetch('data/geodata.json')
+		.then(response => response.json())
+		.then(onGeodataLoaded);
+
+}());
