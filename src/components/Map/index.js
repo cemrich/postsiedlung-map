32ea728f947306a8b3392ce052cb4852e70dd37c
Christine Emrich <christine.emrich@web.de>
2020-01-05 16:19:14 +0100

    Move historic maps into config file

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -6,6 +6,7 @@ import './style.css';
 import 'leaflet_css';
 
 import geoData from './data/geodata.json';
+import historicMapsData from './data/historic-maps.json';
 
 import Category from './Category';
 import OutlineLayer from './layers/OutlineLayer';
@@ -34,27 +35,22 @@ export default class Map extends EventEmitter {
 			[center[0] + panRadius, center[1] + panRadius]
 		]);
 
-		const historicMap1906 = L.imageOverlay('img/maps/map_1906_railway.jpg', [[49.9049487, 8.6132276], [49.8439883, 8.6885816]]);
-		const historicMap1927 = L.imageOverlay('img/maps/Sp_Da1927_0001.jpg',
-			[[49.9015316, 8.6070371], [49.8436596, 8.7044227]],
-			{
-				attribution: 'Karte von <a href="http://tudigit.ulb.tu-darmstadt.de/show/Sp_Da1927/0001">Universitäts- und Landesbibliothek Darmstadt</a>, unter <a href="https://creativecommons.org/publicdomain/zero/1.0/">CC0 1.0</a>'
-			});
-
 		const dataPointsLayer = this._getDataPoints();
 		const todayLayerGroup = L.layerGroup([stamenLayer, outlineLayer]);
-		const historyLayerGroup1906 = L.layerGroup([stamenLayer, historicMap1906, outlineLayer]);
-		const historyLayerGroup1927 = L.layerGroup([stamenLayer, historicMap1927, outlineLayer]);
+
+		this.layerControl = new LayerControl();
+		this.layerControl.addBaseLayer(todayLayerGroup, 'Heute');
+		this.layerControl.addOverlay(dataPointsLayer, 'Orte');
+		this.layerControl.addToMap(this.map);
 
 		this.map.addLayer(todayLayerGroup);
 		this.map.addLayer(dataPointsLayer);
 
-		this.layerControl = new LayerControl();
-		this.layerControl.addBaseLayer(todayLayerGroup, 'Heute');
-		this.layerControl.addBaseLayer(historyLayerGroup1906, '1906');
-		this.layerControl.addBaseLayer(historyLayerGroup1927, '1927');
-		this.layerControl.addOverlay(dataPointsLayer, 'Orte');
-		this.layerControl.addToMap(this.map);
+		for (let historicMap of historicMapsData) {
+			const map = L.imageOverlay('img/maps/' + historicMap.fileName, historicMap.bounds, { attribution: historicMap.attribution });
+			const layerGroup = L.layerGroup([stamenLayer, map, outlineLayer]);
+			this.layerControl.addBaseLayer(layerGroup, historicMap.title);
+		}
 
 		this.map.on('popupopen', e => this.emit('feature-changed', e.popup.feature));
 		this.map.on('popupclose', () => this.emit('feature-changed', null));
